da9af27d826e4bbfc5a4c9cf44ffc1634a70a117
pablojosecode <92952901+pablojosecode@users.noreply.github.com>
2021-12-17 17:04:27 -0800

    Add files via upload

---
+++ Pablo/src/test/java/com/test/Pablo/PabloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.Pablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
